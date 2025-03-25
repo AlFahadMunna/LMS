fb36aacf966b43adc19280a9d2ab1bf8c46f4a32
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-03-25 16:25:18 +0600

    lms project

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
