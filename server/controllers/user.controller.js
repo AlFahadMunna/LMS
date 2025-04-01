8fd19dc2456a687ed4cc69242bc2f3d2b5604548
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-04-01 12:26:46 +0600

    get user profile

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -85,6 +85,17 @@ export const logout = async (_, res) => {
 
 export const getUserProfile = async (req, res) => {
   try {
+    const userId = req.id;
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Profile not found" });
+    }
+    return res.status(200).json({
+      success: true,
+      user,
+    });
   } catch (error) {
     console.log("error while login", error);
     return res.status(500).json({
