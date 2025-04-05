2a3073971da80a570b32a859b711388e22cccd87
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-04-05 12:11:58 +0600

    update profile logic

--- client/src/pages/student/Profile.jsx
+++ client/src/pages/student/Profile.jsx
@@ -12,33 +12,56 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Loader2 } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Course from "./Course";
 import {
   useLoadUserQuery,
   useUpdateUserMutation,
 } from "@/features/api/authApi";
+import { toast } from "sonner";
 
 const Profile = () => {
   const [name, setName] = useState("");
   const [profilePhoto, setProfilePhoto] = useState("");
-  const { data, isLoading } = useLoadUserQuery();
+  const { data, isLoading, refetch } = useLoadUserQuery();
 
   const [
     updateUser,
-    { data: updateUserData, isLoading: updateUserIsLoading, error },
+    {
+      data: updateUserData,
+      isLoading: updateUserIsLoading,
+      isError,
+      error,
+      isSuccess,
+    },
   ] = useUpdateUserMutation();
 
   const onChangeHandler = (e) => {
     const file = e.target.files?.[0];
     if (file) setProfilePhoto(file);
   };
-  if (isLoading) return <h1>Profile Loading...</h1>;
+
+  const updateUserHandler = async () => {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("profilePhoto", profilePhoto);
+    await updateUser(formData);
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      refetch();
+      toast.success(updateUserData?.message || "Profile updated.");
+    }
+    if (isError) {
+      toast.error(error.message || "Failed to update profile");
+    }
+  }, [error, updateUserData, isSuccess, isError]);
+  if (isLoading || !data) return <h1>Profile Loading...</h1>;
   const { user } = data;
-  const updataUserHandler = () => {};
   return (
     <div className="max-w-4xl mx-auto px-4 my-10">
-      <h2 className="font-bold text-2xl text-center md:text-left">PROFILR</h2>
+      <h2 className="font-bold text-2xl text-center md:text-left">PROFILE</h2>
       <div className="flex flex-col md:flex-row items-center md:items-start gap-8 my-5">
         <div className="flex flex-col items-center">
           <Avatar className="h-24 w-24 md:h-32 w-32 mb-4">
@@ -75,7 +98,7 @@ const Profile = () => {
             </h2>
           </div>
           <Dialog>
-            <DialogTrigger asChile>
+            <DialogTrigger asChild>
               <Button className="mt-2">Edit Profile</Button>
             </DialogTrigger>
             <DialogContent>
@@ -108,8 +131,11 @@ const Profile = () => {
                 </div>
               </div>
               <DialogFooter>
-                <Button disabled={isLoading} onClick={updataUserHandler}>
-                  {isLoading ? (
+                <Button
+                  disabled={updateUserIsLoading}
+                  onClick={updateUserHandler}
+                >
+                  {updateUserIsLoading ? (
                     <>
                       <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                       Please wait
