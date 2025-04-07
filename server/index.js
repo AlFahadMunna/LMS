8e7a5a192e12fdb4d6960729c83aa6b5fe77a501
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-04-07 18:32:48 +0600

    create course route and endpoints created

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js";
+import courseRoute from "./routes/course.route.js";
 
 dotenv.config({});
 
@@ -20,6 +21,7 @@ app.use(
 
 //apis
 app.use("/api/v1/user", userRoute);
+app.use("api/v1/course", courseRoute);
 app.listen(PORT, () => {
   console.log(`Server is listening at port: ${PORT}`);
   connectDB();
